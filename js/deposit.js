b081a1451e966781ef8b5876ecacef993a6d2b18
unknown <rasedprogrammer@gmail.com>
2022-09-27 23:22:43 +0600

    adding js bank project

---
+++ js/deposit.js
@@ -0,0 +1,20 @@
+﻿document.getElementById('deposit-btn').addEventListener('click', function (){
+	const depositField = document.getElementById('deposit');
+	const newDepositAmountString = depositField.value;
+	const newDepositAmount = parseFloat(newDepositAmountString);
+
+	// Deposit Balance Total
+	const depositTotalElement = document.getElementById('deposit-total');
+	const previouseDepositTotalString = depositTotalElement.innerText;
+	const previouseDepositTotal = parseFloat(previouseDepositTotalString);
+	const currentDepositTotal = previouseDepositTotal + newDepositAmount;
+	depositTotalElement.innerText = currentDepositTotal;
+	depositField.value = '';
+
+	// Get Balance Total
+	const balanceTotalElement = document.getElementById('balance-total');
+	const previouseBalanceTotalString = balanceTotalElement.innerText;
+	const previouseBalanceTotal = parseFloat(previouseBalanceTotalString);
+	const currentBalanceTotal = previouseBalanceTotal + newDepositAmount;
+	balanceTotalElement.innerText = currentBalanceTotal;
+})
